b32c4f43a5fa3196565c30cb28e99c6d83d2aef9
SusanaJanine <susanajaninevelezflores@gmail.com>
2025-12-31 00:22:30 +0000

    confirm

--- script.js
+++ script.js
@@ -15,3 +15,55 @@ function calculate() {
         display.value = "Error";
     }
 }
+
+function getValue() {
+    return eval(display.value);
+}
+
+function squareRoot() {
+    try {
+        display.value = Math.sqrt(getValue());
+    } catch {
+        display.value = "Error";
+    }
+}
+
+function power() {
+    try {
+        display.value = Math.pow(getValue(), 2);
+    } catch {
+        display.value = "Error";
+    }
+}
+
+function sin() {
+    try {
+        display.value = Math.sin(getValue() * Math.PI / 180);
+    } catch {
+        display.value = "Error";
+    }
+}
+
+function cos() {
+    try {
+        display.value = Math.cos(getValue() * Math.PI / 180);
+    } catch {
+        display.value = "Error";
+    }
+}
+
+function tan() {
+    try {
+        display.value = Math.tan(getValue() * Math.PI / 180);
+    } catch {
+        display.value = "Error";
+    }
+}
+
+function log() {
+    try {
+        display.value = Math.log10(getValue());
+    } catch {
+        display.value = "Error";
+    }
+}
